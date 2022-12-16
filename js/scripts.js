a33778a163352f9f71a46ac80be64adf3b836f59
Lucas Miguel de Arruda <limirruda@gmail.com>
2022-12-16 20:18:57 -0300

    Commit - Publicação

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const paragrafo = document.querySelector('.paragrafo');
+
+document.addEventListener('click', (el) => {
+    const element = el.target;
+
+    if (element.classList.contains('btn')){
+        const extensaoInput = document.querySelector('#extensao').value;
+        const diametroInput = document.querySelector('#diametro').value;
+
+        if (typeof extensaoInput !== 'number' || typeof diametroInput !== 'number') 'Insira somente números';
+
+        const volume = (((diametroInput / 2) / 1000) * 3.14) * extensaoInput;
+        const concentracao = ((volume * 1000) * 200) / 100000;
+
+        paragrafo.innerText = concentracao.toFixed(2) + ' mg/L';
+    }
+})
